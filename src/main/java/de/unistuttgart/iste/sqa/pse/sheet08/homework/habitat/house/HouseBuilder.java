8aa3822511bfbf5b437032982c98c2f24af07175
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-11-23 14:11:51 +0100

    add missing documentation.

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet08/homework/habitat/house/HouseBuilder.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet08/homework/habitat/house/HouseBuilder.java
@@ -8,114 +8,132 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * This class is like the building company that builds the house. At first, all
- * the parts of the house are specified and the house is built by placing walls
- * on the territory's tiles. This class uses the {@link TerritoryBuilder} to
- * place the walls on the territory.
+ * This class is like the building company that builds the house.
+ * At first, all the parts of the house are specified and the house is built by placing walls on the territory's tiles.
+ * <p>
+ * Invariants for this class are, that none of the attributes are null and that the set of walls does not contain any null elements.
  */
 public final class HouseBuilder {
 
-	private final Set<HouseWall> walls;
+    private final Set<HouseWall> walls;
 
-	private final TerritoryBuilder territoryBuilder;
-	private final Territory territory;
+    private final TerritoryBuilder territoryBuilder;
+    private final Territory territory;
 
-	/**
-	 * Creates a new HouseBuilder.
-	 *
-	 * @param territoryBuilder Builder of the territory, required to place the
-	 *                         walls.
-	 * @param territory        The territory itself, required to check it the house
-	 *                         to be build is valid.
-	 */
-	public HouseBuilder(final TerritoryBuilder territoryBuilder, final Territory territory) {
-		this.territoryBuilder = territoryBuilder;
-		this.territory = territory;
+    /**
+     * Creates a new HouseBuilder.
+     * <p>
+     * Requires that neither of the parameters is null.
+     *
+     * @param territoryBuilder Builder of the territory, required to place the walls.
+     * @param territory        The territory itself, required to check it the house to be build is valid.
+     */
+    public HouseBuilder(final TerritoryBuilder territoryBuilder, final Territory territory) {
+        if (territoryBuilder == null || territory == null) {
+            throw new IllegalArgumentException("Neither Territory nor Builder may be null, but one of them is.");
+        }
+        this.territoryBuilder = territoryBuilder;
+        this.territory = territory;
 
-		this.walls = new HashSet<>();
-	}
+        this.walls = new HashSet<>();
+    }
 
-	/**
-	 * Adds a new wall to the house that will be built.
-	 *
-	 * @param housewall the new wall.
-	 * @return The builder for chaining.
-	 */
-	public HouseBuilder withWall(final HouseWall housewall) {
-		if (this.wallsOverlap(housewall)) {
-			throw new IllegalArgumentException("New wall overlaps with existing wall.");
-		}
-		this.walls.add(housewall);
-		return this;
-	}
+    /**
+     * Adds a new wall to the house that will be built.
+     * <p>
+     * Requires, that the new wall is not null and that it does not overlap with any of the already planned walls.
+     *
+     * @param wall the new wall.
+     * @return The builder for chaining.
+     */
+    public HouseBuilder withWall(final HouseWall wall) {
+        if (wall == null) {
+            throw new IllegalArgumentException("New house wall must not be null.");
+        }
+        if (this.wallsOverlap(wall)) {
+            throw new IllegalArgumentException("New house wall overlaps with existing house wall.");
+        }
+        this.walls.add(wall);
+        return this;
+    }
 
-	/**
-	 * Creates the house with the specified walls by placing walls on the
-	 * territory's tiles. There must be at least one wall specified.
-	 *
-	 * @return The house, that is now also visible on the territory.
-	 */
-	public House build() {
-		if (walls.isEmpty()) {
-			throw new IllegalStateException("No walls specified.");
-		}
-        for (HouseWall wall: walls) {
-            if (this.wallIsBlockedOnTerritory(wall)) {
-            	throw new IllegalStateException("Location on Territory is blocked, cannot build house.");
+
+    /**
+     * Creates the house with the specified walls by placing walls on the territory's tiles.
+     * There must be at least one wall specified.
+     *
+     * @return The house, that is now also visible on the territory.
+     */
+    public House build() {
+        if (walls.isEmpty()) {
+            throw new IllegalStateException("No walls specified.");
+        }
+        for (HouseWall wall : walls) {
+            if (this.isTileOfWallBlockedOnTerritory(wall)) {
+                throw new IllegalStateException("Location on Territory is blocked, cannot build house.");
             }
         }
-		for (HouseWall wall : walls) {
-			buildWall(wall);
-		}
-		return new House(walls);
-	}
+        for (HouseWall wall : walls) {
+            buildWall(wall);
+        }
+        return new House(walls);
+    }
 
-	/**
-	 * Create a single housewall by placing walls on the territory's tiles.
-	 *
-	 * @param housewall the wall to be put on the territory.
-	 */
-	private void buildWall(final HouseWall housewall) {
+    /**
+     * Create a single housewall by placing walls on the territory's tiles.
+     * <p>
+     * Requires that the housewall is not null.
+     *
+     * @param wall the housewall to be put on the territory.
+     */
+    private void buildWall(final HouseWall wall) {
 		// TODO implement exercise 2 (d) here.
-	}
+    }
 
-	/**
-	 * Check whether the new housewall overlaps with any of the already planned
-	 * housewalls or with anything that is already placed on the territory.
-	 *
-	 * @param newWall the new housewall.
-	 * @return true if the new housewall overlaps with anything.
-	 */
-	private boolean wallsOverlap(final HouseWall newWall) {
-		for (HouseWall wall : walls) {
-			if (wall.overlapsWith(wall)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Check whether the new wall overlaps with any of the already planned walls or with anything that is already placed on the territory.
+     * <p>
+     * Requires that the new wall is not null.
+     *
+     * @param newWall the new wall.
+     * @return true if the new wall overlaps with anything.
+     */
+    private boolean wallsOverlap(final HouseWall newWall) {
+        assert newWall != null;
+        for (HouseWall wall : walls) {
+            if (wall.overlapsWith(wall)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * Checks whether any of the tiles that the housewall would cover are already blocked
-	 * on the territory, e.g. by a grain, a hamster or another wall.
-	 *
-	 * @param wall the new wall.
-	 * @return true if the wall would cover a blocked tile, false otherwise.
-	 */
-	private boolean wallIsBlockedOnTerritory(final HouseWall wall) {
-		if (wall.isVertical()) {
-			for (int i = wall.getStart().getRow(); i < wall.getEnd().getRow(); i++) {
-				if (!territory.isFree(Location.from(i, wall.getStart().getColumn()))) {
-					return true;
-				}
-			}
-		} else {
-			for (int i = wall.getStart().getColumn(); i < wall.getEnd().getColumn(); i++) {
-				if (!territory.isFree(Location.from(wall.getStart().getRow(), i))) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    /**
+     * Checks whether any of the tiles that the housewall would cover is already blocked
+     * on the territory.
+     * <p>
+     * Requires that wall is not null.
+     *
+     * @param wall the new wall.
+     * @return true if the wall would cover a blocked tile, false otherwise.
+     */
+    private boolean isTileOfWallBlockedOnTerritory(final HouseWall wall) {
+        assert wall != null;
+        if (wall.isVertical()) {
+            for (int i = wall.getStart().getRow(); i < wall.getEnd().getRow(); i++) {
+                if (!territory.isFree(Location.from(i, wall.getStart().getColumn()))) {
+                    return true;
+                }
+            }
+        } else {
+            for (int i = wall.getStart().getColumn(); i < wall.getEnd().getColumn(); i++) {
+                if (!territory.isFree(Location.from(wall.getStart().getRow(), i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet08/homework/habitat/house/HouseWall.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet08/homework/habitat/house/HouseWall.java
@@ -9,208 +9,329 @@ import java.util.Optional;
  * 
  * All walls in a {@link House}, must adhere to the following invariants:
  * <p>
- * Every housewall consist of a start and end location, both are inclusive, i.e. the
- * start and the end location are part of the housewall. The start location must be
- * closer to the territories origin (the upper left corner) than the end
+ * Every housewall consist of a start and end location, both are inclusive, i.e.
+ * the start and the end location are part of the housewall. The start location
+ * must be closer to the territories origin (the upper left corner) than the end
  * location. In addition, the start and end location must be in line, i.e. for a
- * horizontal housewall, they must be in the same row, and for a vertical housewall, 
- * they must be in the same column.
+ * horizontal housewall, they must be in the same row, and for a vertical
+ * housewall, they must be in the same column.
  *
  * <p>
- * A housewall must at least be two tiles long, i.e. start and end must not be the
- * same location.
+ * A housewall must at least be two tiles long, i.e. start and end must not be
+ * the same location.
  *
  * <p>
- * In addition, housewall have an optional door. If there is no door, i.e. the 
- * door is an empty {@code Optional}, no further restrictions apply. If the 
- * housewall has a door, that door must be located in between start and end.
- * In addition, the door must not be at the very start or end of the housewall.
+ * In addition, housewall have an optional door. If there is no door, i.e. the
+ * door is an empty {@code Optional}, no further restrictions apply. If the
+ * housewall has a door, that door must be located in between start and end. In
+ * addition, the door must not be at the very start or end of the housewall.
  * 
  */
 public final class HouseWall {
-    private final Location start;
-    private final Location end;
+	private final Location start;
+	private final Location end;
 
-    private Optional<Location> door;
+	private Optional<Location> door;
 
 	/**
 	 * Constructs a new wall, which may or may not have a door.
 	 * 
-	 * Requires that neither start nor end are null and that start and end form a valid housewall, according to the class' invariants.
-	 * Ensures the creation of a new housewall according to the class' invariants. 
+	 * Requires that neither start nor end are null and that start and end form a
+	 * valid housewall, according to the class' invariants. Ensures the creation of
+	 * a new housewall according to the class' invariants.
 	 *
 	 * @param start location of the wall's first part
 	 * @param end   location of the wall's last part
 	 */
-    public HouseWall(final Location start, final Location end) {
-    	// TODO implement exercise 2 (a) here. 
-    	
-    	
-        this.start = start;
-        this.end = end;
-        this.door = Optional.empty();
-    }
-    
+	public HouseWall(final Location start, final Location end) {
+		// TODO implement exercise 2 (a) here.
+
+		this.start = start;
+		this.end = end;
+		this.door = Optional.empty();
+	}
+
 	/**
-	 * Add a door into a house wall. 
+	 * Add a door into a house wall.
 	 * 
-	 * Requires that {@code newDoor} is not null, and that {@code newDoor} is a valid door according to the class' invariants. 
-	 * Additionally requires that the housewall does not yet have a door, i.e. {@code this.door.isEmpty()}.
+	 * Requires that {@code newDoor} is not null, and that {@code newDoor} is a
+	 * valid door according to the class' invariants. Additionally requires that the
+	 * housewall does not yet have a door, i.e. {@code this.door.isEmpty()}.
 	 * 
 	 * Ensures that {@code this.door} is not empty.
 	 * 
 	 * @param newDoor door to be added into the housewall.
 	 */
 	public void addDoor(final Location newDoor) {
-		// TODO implement exercise 2 (b) here. 
-	}
-    
-
-    /**
-     * Get the first tile of the wall.
-     *
-     * @return the first tile of the wall.
-     */
-    public Location getStart() {
-        return start;
-    }
-
-    /**
-     * Get the last tile of the wall.
-     *
-     * @return the last tile of the wall.
-     */
-    public Location getEnd() {
-        return end;
-    }
-
-    /**
-     * Get the door of the wall.
-     *
-     * @return the door of the wall.
-     */
-    public Optional<Location> getDoor() {
-        return door;
-    }
-
-    /**
-     * Checks if the wall is vertical.
-     *
-     * @return true if the wall is vertical, false otherwise.
-     */
-    public boolean isVertical() {
-        return start.getColumn() == end.getColumn();
-    }
-
-    /**
-     * Checks if the wall is horizontal.
-     *
-     * @return true if the wall is horizontal, false otherwise.
-     */
-    public boolean isHorizontal() {
-        return start.getRow() == end.getRow();
-    }
-    
-    
-    /*
-     * Checks for overlaps among walls.  
-     */
-    
-    /**
-	 * Checks whether {@code otherWall} overlaps with this housewall.
-	 * 
-	 * @param otherWall another housewall
-	 * @return true if the housewalls overlap.
-	 */
-	public boolean overlapsWith(final HouseWall otherWall) {
-		return this.horizontalWallsOverlap(otherWall) || this.verticallyOverlapsWith(otherWall)
-				|| this.intersectionWallsOverlap(otherWall);
+		// TODO implement exercise 2 (b) here.
 	}
 
 	/**
-	 * Checks whether another housewall vertically overlaps with the current housewall.
-	 * 
-	 * @param otherWall another housewall
-	 * @return true if the housewalls overlap vertically, false otherwise.
-	 */
-	private boolean verticallyOverlapsWith(final HouseWall otherWall) {
-		if (!(this.isVertical() && otherWall.isVertical())) { 
-			return false;
-		}
-		return isInSameCollumn(otherWall)
-				&& (isStartRowWithinOtherWall(otherWall) || isEndRowWithinOtherWall(otherWall));
-	}
-
-	private boolean isInSameCollumn(final HouseWall otherWall) {
-		return start.getColumn() != otherWall.getStart().getColumn();
-	}
-
-	private boolean isEndRowWithinOtherWall(final HouseWall otherWall) {
-		return end.getRow() >= otherWall.getStart().getRow() && end.getRow() <= otherWall.getStart().getRow();
-	}
-
-	private boolean isStartRowWithinOtherWall(final HouseWall otherWall) {
-		return start.getRow() >= otherWall.getStart().getRow() && start.getRow() <= otherWall.getEnd().getRow();
-	}
-
-	/**
-	 * Checks whether another housewall horizontally overlaps with the current housewall.
+	 * Get the first tile of the wall.
 	 *
-	 * @param otherWall another housewall
-	 * @return true if the housewalls overlap horizontally, false otherwise.
+	 * @return the first tile of the wall.
 	 */
-	private boolean horizontalWallsOverlap(final HouseWall otherWall) {
-		if (!(this.isHorizontal() && otherWall.isHorizontal())) { // one is not horizontal.
-			return false;
-		}
-		return this.isInSameRow(otherWall)
-				&& (isStartCollumnWithinOtherWall(otherWall) || isEndCollumnWithinOtherWall(otherWall));
-	}
-
-	private boolean isInSameRow(final HouseWall otherWall) {
-		return start.getRow() != otherWall.getStart().getRow();
-	}
-
-	private boolean isEndCollumnWithinOtherWall(final HouseWall otherWall) {
-		return end.getColumn() >= otherWall.getStart().getColumn()
-				&& end.getColumn() <= otherWall.getStart().getColumn();
-	}
-
-	private boolean isStartCollumnWithinOtherWall(final HouseWall otherWall) {
-		return start.getColumn() >= otherWall.getStart().getColumn()
-				&& start.getColumn() <= otherWall.getEnd().getColumn();
+	public Location getStart() {
+		return start;
 	}
 
 	/**
-	 * Checks whether another housewall intersects with the current housewall.
-	 * 
-	 * @param otherWall the other housewall
-	 * @return true if the housewalls overlap, false otherwise.
+	 * Get the last tile of the wall.
+	 *
+	 * @return the last tile of the wall.
 	 */
-	private boolean intersectionWallsOverlap(final HouseWall otherWall) {
-		final HouseWall horizontalWall;
-		final HouseWall verticalWall;
-
-		if (this.isHorizontal() && otherWall.isVertical()) {
-			horizontalWall = this;
-			verticalWall = otherWall;
-		} else if (this.isVertical() && otherWall.isHorizontal()) {
-			horizontalWall = otherWall;
-			verticalWall = this;
-		} else {
-			return false;
-		}
-
-		for (int i = horizontalWall.getStart().getColumn(); i <= horizontalWall.getEnd().getColumn(); i++) {
-			Location horizontalLoc = Location.from(horizontalWall.getStart().getRow(), i);
-
-			for (int j = verticalWall.getStart().getRow(); j <= verticalWall.getEnd().getRow(); j++) {
-				Location verticalLoc = Location.from(j, verticalWall.getStart().getColumn());
-				if (verticalLoc.equals(horizontalLoc)) {
-					return true;
-				}
-			}
-		}
-		return false;
+	public Location getEnd() {
+		return end;
 	}
+
+	/**
+	 * Get the door of the wall.
+	 *
+	 * @return the door of the wall.
+	 */
+	public Optional<Location> getDoor() {
+		return door;
+	}
+
+	/**
+	 * Checks if the wall is vertical.
+	 *
+	 * @return true if the wall is vertical, false otherwise.
+	 */
+	public boolean isVertical() {
+		return start.getColumn() == end.getColumn();
+	}
+
+	/**
+	 * Checks if the wall is horizontal.
+	 *
+	 * @return true if the wall is horizontal, false otherwise.
+	 */
+	public boolean isHorizontal() {
+		return start.getRow() == end.getRow();
+	}
+
+    /**
+     * Checks whether the given start and end locations are in line.
+     * <p>
+     * Requires that start and end are not null.
+     *
+     * @param start first tile of the wall
+     * @param end   last tile of the wall
+     * @return true iff the start and end location are in line.
+     */
+    private boolean areInLine(final Location start, final Location end) {
+        assert start != null && end != null;
+        return start.getColumn() == end.getColumn() || start.getRow() == end.getRow();
+    }
+
+    /**
+     * Checks whether the given start location is closer to the origin than the end location.
+     * <p>
+     * Requires that start and end are not null.
+     *
+     * @param start first tile of the wall
+     * @param end   last tile of the wall
+     * @return true iff the start location is closer to the origin than the end location.
+     */
+    private boolean isStartSmallerThanEnd(final Location start, final Location end) {
+        assert start != null && end != null;
+        return (start.getRow() == end.getRow() && start.getColumn() < end.getColumn())
+                || (start.getColumn() == end.getColumn() && start.getRow() < end.getRow());
+    }
+
+    /**
+     * Checks whether the door is an a valid position on a vertical wall.
+     * <p>
+     * Requires that door is not null.
+     *
+     * @param door tile of the door
+     * @return true iff the door has a valid position on a vertical wall.
+     */
+    private boolean isValidDoorOnVerticalWall(final Location door) {
+        assert door != null;
+        return this.isVertical() && (door.getColumn() == start.getColumn() && door.getRow() > start.getRow()
+                && door.getRow() < end.getRow());
+    }
+
+    /**
+     * Checks whether the door is an a valid position on a horizontal wall.
+     * <p>
+     * Requires that door is not null.
+     *
+     * @param door tile of the door
+     * @return true iff the door has a valid position on a horizontal wall.
+     */
+    private boolean isValidDoorOnHorizontalWall(final Location door) {
+        assert door != null;
+        return this.isHorizontal() && (door.getRow() == start.getRow() && door.getColumn() > start.getColumn()
+                && door.getColumn() < end.getColumn());
+    }
+
+    /**
+     * Checks whether the current wall overlaps with another wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the walls overlap, false otherwise.
+     */
+    public boolean overlapsWith(final HouseWall otherWall) {
+        if (otherWall == null) {
+            throw new IllegalArgumentException("Parameter is null, but must not be.");
+        }
+        return this.horizontalWallsOverlap(otherWall) || this.verticallyOverlapsWith(otherWall)
+                || this.intersectionWallsOverlap(otherWall);
+    }
+
+    /**
+     * Checks whether another wall vertically overlaps with the current wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the walls overlap vertically, false otherwise.
+     */
+    private boolean verticallyOverlapsWith(final HouseWall otherWall) {
+        assert otherWall != null;
+        if (!(this.isVertical() && otherWall.isVertical())) {
+            return false;
+        }
+        return isInSameColumn(otherWall)
+                && (isStartRowWithinOtherWall(otherWall) || isEndRowWithinOtherWall(otherWall));
+    }
+
+
+    /**
+     * Checks whether the given wall is in the same column as the current wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the walls are in the same column, false otherwise.
+     */
+    private boolean isInSameColumn(final HouseWall otherWall) {
+        assert otherWall != null;
+        return start.getColumn() != otherWall.getStart().getColumn();
+    }
+
+    /**
+     * Checks whether the row of the end of the current wall is within the other wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the row of the end of the current wall is within the other wall, false otherwise.
+     */
+    private boolean isEndRowWithinOtherWall(final HouseWall otherWall) {
+        assert otherWall != null;
+        return end.getRow() >= otherWall.getStart().getRow() && end.getRow() <= otherWall.getStart().getRow();
+    }
+
+    /**
+     * Checks whether the row of the start of the current wall is within the other wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the row of the start of the current wall is within the other wall, false otherwise.
+     */
+    private boolean isStartRowWithinOtherWall(final HouseWall otherWall) {
+        assert otherWall != null;
+        return start.getRow() >= otherWall.getStart().getRow() && start.getRow() <= otherWall.getEnd().getRow();
+    }
+
+    /**
+     * Checks whether another wall horizontally overlaps with the current wall.
+     *
+     * @param otherWall another wall
+     * @return true if the walls overlap horizontally, false otherwise.
+     */
+    private boolean horizontalWallsOverlap(final HouseWall otherWall) {
+        assert otherWall != null;
+        if (!(this.isHorizontal() && otherWall.isHorizontal())) { // one is not horizontal.
+            return false;
+        }
+        return this.isInSameRow(otherWall)
+                && (isStartColumnWithinOtherWall(otherWall) || isEndColumnWithinOtherWall(otherWall));
+    }
+
+    /**
+     * Checks whether the given wall is in the same row as the current wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the walls are in the same row, false otherwise.
+     */
+    private boolean isInSameRow(final HouseWall otherWall) {
+        assert otherWall != null;
+        return start.getRow() != otherWall.getStart().getRow();
+    }
+
+    /**
+     * Checks whether the column of the end of the current wall is within the other wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the column of the end of the current wall is within the other wall, false otherwise.
+     */
+    private boolean isEndColumnWithinOtherWall(final HouseWall otherWall) {
+        assert otherWall != null;
+        return end.getColumn() >= otherWall.getStart().getColumn()
+                && end.getColumn() <= otherWall.getStart().getColumn();
+    }
+
+    /**
+     * Checks whether the column of the start of the current wall is within the other wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall another wall
+     * @return true if the column of the start of the current wall is within the other wall, false otherwise.
+     */
+    private boolean isStartColumnWithinOtherWall(final HouseWall otherWall) {
+        assert otherWall != null;
+        return start.getColumn() >= otherWall.getStart().getColumn()
+                && start.getColumn() <= otherWall.getEnd().getColumn();
+    }
+
+    /**
+     * Checks whether another wall intersects with the current wall.
+     * <p>
+     * Requires that otherWall is not null.
+     *
+     * @param otherWall the other wall
+     * @return true if the walls overlap, false otherwise.
+     */
+    private boolean intersectionWallsOverlap(final HouseWall otherWall) {
+        assert otherWall != null;
+
+        final HouseWall horizontalWall;
+        final HouseWall verticalWall;
+
+        if (this.isHorizontal() && otherWall.isVertical()) {
+            horizontalWall = this;
+            verticalWall = otherWall;
+        } else if (this.isVertical() && otherWall.isHorizontal()) {
+            horizontalWall = otherWall;
+            verticalWall = this;
+        } else {
+            return false;
+        }
+
+        for (int i = horizontalWall.getStart().getColumn(); i <= horizontalWall.getEnd().getColumn(); i++) {
+            Location horizontalLoc = Location.from(horizontalWall.getStart().getRow(), i);
+
+            for (int j = verticalWall.getStart().getRow(); j <= verticalWall.getEnd().getRow(); j++) {
+                Location verticalLoc = Location.from(j, verticalWall.getStart().getColumn());
+                if (verticalLoc.equals(horizontalLoc)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
