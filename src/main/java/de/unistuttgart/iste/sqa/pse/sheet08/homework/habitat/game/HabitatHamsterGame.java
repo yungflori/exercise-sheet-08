59be27d114c907b7311fc947768599b62a1b4892
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-11-28 12:26:34 +0100

    implement run to check correct position of walls and doors.

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet08/homework/habitat/game/HabitatHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet08/homework/habitat/game/HabitatHamsterGame.java
@@ -33,6 +33,63 @@ public final class HabitatHamsterGame extends SimpleHamsterGame {
 
 	@Override
 	protected void run() {
+		turnRight();
+		multiMove(7);
+		paule.turnLeft();
+		multiMove(11);
+		paule.turnLeft();
+		multiMove(3);
+		checkDoor();
 		
+		multiMove(3);
+		paule.turnLeft();
+		multiMove(3);
+		turnRight();
+		paule.move();
+		paule.turnLeft();
+		multiMove(5);
+		checkDoor();
+		multiMove(3);
+
+		paule.write("Wuhu, i ran around my house!");
+	}
+
+	/**
+	 * Paule turns to the right.
+	 */
+	private void turnRight() {
+		paule.turnLeft();
+		paule.turnLeft();
+		paule.turnLeft();
+	}
+
+	/**
+	 * Paule checks whether he's standing next to a door.
+	 * 
+	 * Ensure, that paule looks into the initial direction.
+	 */
+	private void checkDoor() {
+		paule.turnLeft();
+		if (!paule.frontIsClear()) {
+			throw new IllegalStateException("Door is blocked!!");
+		}
+		turnRight();
+	}
+
+	/**
+	 * Paule move the given number of steps.
+	 * 
+	 * Requires, that {@code numberOfSteps} in front of paule are clear.
+	 * 
+	 * @param numberOfSteps number of steps to move
+	 */
+	private void multiMove(int numberOfSteps) {
+		/*@
+		 @ loop_invariant paule moved i steps
+		 @ decreasing numberOfSteps - i 
+		 @*/
+		for (int i = 0; i < numberOfSteps; i++) {
+			paule.move();
+		}
 	}
 }
